e1f4904f9a446f45e3e4aacec8bbf0f5fb9b397a
Trailgenic <mike@trailgenic.com>
2026-04-09 16:57:46 -0700

    Delete lib directory

--- lib/store.ts
+++
@@ -1,9 +0,0 @@
-import type { IntakePayload } from "./ella";
-
-const globalStore = globalThis as typeof globalThis & {
-  __exitdeskIntakeStore?: Map<string, IntakePayload>;
-};
-
-export const intakeStore: Map<string, IntakePayload> =
-  globalStore.__exitdeskIntakeStore ??
-  (globalStore.__exitdeskIntakeStore = new Map());
--- lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from "stripe";
-
-export const PRICE = 49900;
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2026-03-25.dahlia"
-});
